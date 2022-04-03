45efad08def9d79feff0768083634ed9299375d9
Josue Zambrano <josuezambrano2104@gmail.com>
2022-04-03 16:46:14 -0300

    Creacion de switch, monto total y mejora de constructor

--- script.js
+++ script.js
@@ -19,12 +19,47 @@ class Catalogo {
         this.precio = precio;
     }
     descripcion () {
-        alert (`tenemos ${this.stock} ${this.categoria} en stock, a un precio accesible de ${this.precio} pesos`);
+        alert (`tenemos ${this.stock} ${this.categoria} en stock, a un precio accesible de ${this.precio} pesos c/u`);
+    }
+    preguntaVendedor () {
+        let respuesta = parseInt(prompt ('Cuanta cantidad desea comprar??'));
+        while ((respuesta == 0) || (respuesta > `${this.stock}`)) {
+            alert ('Cantidad invalida o supera el monto en stock disponible');
+            respuesta = parseInt(prompt ('Cuanta cantidad desea comprar??'));
+        }
+        let precioFinal = respuesta * `${this.precio}`;
+        alert (`Excelente, el monto total a pagar quedaria en ${precioFinal} pesos!, muchas gracias por su compra!`);
     }
 }
 const instrumentos = new Catalogo ('guitarras', 3, 5000);
 const equipos = new Catalogo ('amplificadores', 5, 2000);
 const accesorios = new Catalogo ('Cuerdas', 10, 200);
+const indumentaria = new Catalogo ('remeras', 30, 500);
 console.log(instrumentos);
 console.log(equipos)
 console.log(accesorios);
+console.log(indumentaria);
+
+//SELECCION DE COMPRA DE CATALOGO
+let usuarioCompra = prompt ('Quieres comprar guitarras, amplificadores o cuerdas??, tambien tenemos indumentaria');
+switch (usuarioCompra) {
+    case "guitarras":
+        instrumentos.descripcion();
+        instrumentos.preguntaVendedor();
+        break;
+    case "amplificadores":
+        equipos.descripcion();
+        equipos.preguntaVendedor();
+        break;
+    case "cuerdas":
+        accesorios.descripcion();
+        accesorios.preguntaVendedor();
+        break;
+    case "indumentaria":
+        indumentaria.descripcion();
+        indumentaria.preguntaVendedor();
+        break;
+    default:
+        alert('Lo siento, ya no tenemos de eso');
+        break;
+}
