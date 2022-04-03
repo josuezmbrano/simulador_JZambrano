95042b9c94f7f3402046a9b878f287b43ef4e603
Josue Zambrano <josuezambrano2104@gmail.com>
2022-04-03 15:11:21 -0300

    Creacion de Catalogo en JS

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function registrarUsuario () {
 let nombreUsuario = prompt('Cual es tu nombre??');
 let apellidoUsuario = prompt ('Cual es tu apellido??');
 registrarUsuario ();
+
+//CATALOGO DE COMPRAS
+class Catalogo {
+    constructor(categoria, stock, precio) {
+        this.categoria = categoria;
+        this.stock = stock;
+        this.precio = precio;
+    }
+    descripcion () {
+        alert (`tenemos ${this.stock} ${this.categoria} en stock, a un precio accesible de ${this.precio} pesos`);
+    }
+}
+const instrumentos = new Catalogo ('guitarras', 3, 5000);
+const equipos = new Catalogo ('amplificadores', 5, 2000);
+const accesorios = new Catalogo ('Cuerdas', 10, 200);
+console.log(instrumentos);
+console.log(equipos)
+console.log(accesorios);
